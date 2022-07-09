5cc490a81d7d22c028c608e78716ecf658b3e015
danielpqb <danielpqb@gmail.com>
2022-07-09 18:42:24 -0300

    Fix to CSS path

--- index.js
+++ index.js
@@ -18,15 +18,15 @@ function selectCategory() {
     switch (Number(this.id)) {
         case 1: //Resume
             document.querySelector('.page').innerHTML = resumeContent
-            document.querySelector('.cssFile').innerHTML = `<link rel="stylesheet" href="resume.css"></link>`
+            document.querySelector('.cssFile').innerHTML = `<link rel="stylesheet" href="css/resume.css"></link>`
             break;
         case 2: //Portfolio
             document.querySelector('.page').innerHTML = portfolioContent
-            document.querySelector('.cssFile').innerHTML = `<link rel="stylesheet" href="portfolio.css"></link>`
+            document.querySelector('.cssFile').innerHTML = `<link rel="stylesheet" href="css/portfolio.css"></link>`
             break;
         case 3: //Contact
             document.querySelector('.page').innerHTML = contactContent
-            document.querySelector('.cssFile').innerHTML = `<link rel="stylesheet" href="contact.css"></link>`
+            document.querySelector('.cssFile').innerHTML = `<link rel="stylesheet" href="css/contact.css"></link>`
             break;
         default: //About Me
             document.querySelector('.page').innerHTML = aboutmeContent
